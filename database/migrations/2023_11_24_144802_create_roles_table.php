835c48ce7a896087b4cc7ae4f74e451ba51b66ef
kazburrel <oegbosionu@gmail.com>
2023-11-24 15:59:14 +0100

    setup

--- database/migrations/2023_11_24_144802_create_roles_table.php
+++ database/migrations/2023_11_24_144802_create_roles_table.php
@@ -12,8 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('roles', function (Blueprint $table) {
-            $table->id();
-            $table->uuid('unique_id');
+            $table->uuid('id')->primary();
             $table->string('name');
             $table->timestamps();
             $table->softDeletes();
