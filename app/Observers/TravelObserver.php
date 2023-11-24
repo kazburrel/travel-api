fd6160a2b2540d694790ea857d5508c6e66b9d1c
kazburrel <oegbosionu@gmail.com>
2023-11-24 16:32:30 +0100

    db_setup

---
+++ app/Observers/TravelObserver.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Travel;
+
+class TravelObserver
+{
+    /**
+     * Handle the Travel "created" event.
+     */
+    public function creating(Travel $travel): void
+    {
+        //
+    }
+
+   
+}
