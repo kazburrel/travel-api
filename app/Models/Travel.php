42b2f8e087a334aae8a7ff1464fba35fee10b9ae
kazburrel <oegbosionu@gmail.com>
2023-11-24 17:28:06 +0100

    tours db setup

--- app/Models/Travel.php
+++ app/Models/Travel.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Travel extends Model
@@ -20,6 +21,11 @@ class Travel extends Model
         'description',
         'number_of_days',
     ];
+
+    public function tours(): HasMany
+    {
+        return $this->hasMany(Tour::class);
+    }
 
     public function sluggable(): array
     {
