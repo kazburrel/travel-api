6a39e4ef7e76f152bb5fd5d9f7ef3e93765f8de2
kazburrel <oegbosionu@gmail.com>
2023-11-24 15:54:24 +0100

    setup

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Role extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids, SoftDeletes;
+
+    protected $fillable = ['name'];
 }
