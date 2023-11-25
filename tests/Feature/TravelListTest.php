af47c1408e65293bcf4e373b6ec118ff3b4d7af7
kazburrel <oegbosionu@gmail.com>
2023-11-25 13:03:21 +0100

    feature test for travels

--- tests/Feature/TravelListTest.php
+++ tests/Feature/TravelListTest.php
@@ -13,9 +13,9 @@ class TravelListTest extends TestCase
 
     public function test_travels_list_returns_paginated_data_correctly(): void
     {
-        Travel::factory(16)->create();
+        Travel::factory(16)->create(['is_public' => true]);
 
-        $response = $this->get('/');
+        $response = $this->get('/api/v1/travels');
 
         $response->assertStatus(200);
         $response->assertJsonCount(15, 'data');
