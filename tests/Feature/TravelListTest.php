6f52a506c983fdc2ab6e4a2fa9ccea690f2f3493
kazburrel <oegbosionu@gmail.com>
2023-11-25 13:29:50 +0100

    feature test for travels

--- tests/Feature/TravelListTest.php
+++ tests/Feature/TravelListTest.php
@@ -16,9 +16,25 @@ class TravelListTest extends TestCase
         Travel::factory(16)->create(['is_public' => true]);
 
         $response = $this->get('/api/v1/travels');
-
         $response->assertStatus(200);
         $response->assertJsonCount(15, 'data');
         $response->assertJsonPath('meta.last_page', 2);
+    }
+
+    public function test_travels_list_shows_only_public_records()
+    {
+        // We create two Travels: one public and one private
+        $nonPublicTravel = Travel::factory()->create(['is_public' => false]);
+        $publicTravel = Travel::factory()->create(['is_public' => true]);
+ 
+        $response = $this->get('/api/v1/travels');
+ 
+        $response->assertStatus(200);
+ 
+        // We check that only the public record is returned
+        $response->assertJsonCount(1, 'data');
+        $response->assertJsonFragment(['id' => $publicTravel->id]);
+        $response->assertJsonMissing(['id' => $nonPublicTravel->id]);
+ 
     }
 }
