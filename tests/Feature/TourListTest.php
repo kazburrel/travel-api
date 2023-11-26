95329b808efeb2325d12d98ea84efa6f8bd172f5
kazburrel <oegbosionu@gmail.com>
2023-11-26 23:19:03 +0100

    tour feature test

--- tests/Feature/TourListTest.php
+++ tests/Feature/TourListTest.php
@@ -2,19 +2,24 @@
 
 namespace Tests\Feature;
 
+use App\Models\Tour;
+use App\Models\Travel;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class TourListTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
+    use RefreshDatabase;
+
+    public function test_tours_list_by_travel_slug_returns_correct_tours(): void
     {
-        $response = $this->get('/');
+        $travel = Travel::factory()->create();
+        $tour = Tour::factory()->create('travel_id', $travel->id);
+        $response = $this->get('/api/v1/travels/' . $travel->slug . '/tours');
 
         $response->assertStatus(200);
+        $response->assertJsonCount(1, 'data');
+        $response->assertJsonFragment(['id' => $tour->id]);
     }
 }
