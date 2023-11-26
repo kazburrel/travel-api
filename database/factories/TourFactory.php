c36889e9d6050c74898c563b05d736fc50c12c58
kazburrel <oegbosionu@gmail.com>
2023-11-26 23:42:41 +0100

    tour feature test

--- database/factories/TourFactory.php
+++ database/factories/TourFactory.php
@@ -4,9 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Travel;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Carbon;
 use Illuminate\Support\Str;
-
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tour>
  */
@@ -19,14 +17,13 @@ class TourFactory extends Factory
      */
     public function definition(): array
     {
-        $randomDate = fake()->date;
         return [
             'id' => Str::uuid(),
             'travel_id' => Travel::inRandomOrder()->first()->id,
             'name' => fake()->text(10),
-            'starting_date' => $randomDate,
-            'ending_date' => Carbon::parse($randomDate)->addDays(5)->toDateString(),
-            'price' => fake()->numberBetween(10, 100),
+            'starting_date' => now(),
+            'ending_date' => now()->addDays(rand(1, 10)),
+            'price' => fake()->randomFloat(2, 10, 999),
         ];
     }
 }
--- tests/Feature/TourListTest.php
+++ tests/Feature/TourListTest.php
@@ -15,11 +15,12 @@ class TourListTest extends TestCase
     public function test_tours_list_by_travel_slug_returns_correct_tours(): void
     {
         $travel = Travel::factory()->create();
-        $tour = Tour::factory()->create('travel_id', $travel->id);
+        $tour = Tour::factory()->create(['travel_id'=> $travel->id]);
         $response = $this->get('/api/v1/travels/' . $travel->slug . '/tours');
 
         $response->assertStatus(200);
         $response->assertJsonCount(1, 'data');
         $response->assertJsonFragment(['id' => $tour->id]);
     }
+
 }
