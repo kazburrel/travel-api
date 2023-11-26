9c5def2934b1ef45aa4f97acbe40ff65bd8af7c7
kazburrel <oegbosionu@gmail.com>
2023-11-26 19:18:06 +0100

    tour feature

--- app/Http/Controllers/Api/V1/TourController.php
+++ app/Http/Controllers/Api/V1/TourController.php
@@ -3,9 +3,19 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\TourResource;
+use App\Models\Tour;
+use App\Models\Travel;
 use Illuminate\Http\Request;
 
 class TourController extends Controller
 {
-    //
+    public function index(Travel $travel)
+    {
+        $tours = $travel->tours() //Tour::where('travel_id', $travel->id) both are the same
+            ->orderBy('starting_date')
+            ->paginate();
+
+        return TourResource::collection($tours);
+    }
 }
